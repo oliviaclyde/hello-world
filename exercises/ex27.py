7902ee3279a63501221841ee1741f2966ceba200
oliviaclyde <olivialclyde@gmail.com>
2019-05-29 06:21:34 -0600

    initial

---
+++ exercises/ex27.py
@@ -0,0 +1,47 @@
+#Memeorizing logic
+#The Truth Tables
+
+
+#NOT                    True or False?
+#not False              True
+#not True               False
+
+
+#OR                     True or False?
+#True or False          True
+#True or True           True
+#False or True          True
+#False or False         False
+
+
+#AND                    True or False?
+#True and False         False
+#True and True          True
+#False and True         False
+#False and False        False
+
+
+#NOT OR                 True or False?
+#not (True or False)    False
+#not (True or True)     False
+#not (False or True)    False
+#not (False or False)   True
+
+
+#NOT AND                True or False?
+#not (True and False)   True
+#not (True and True)    False
+#not (False and True)   True
+#not (False and False)  True
+
+#!=                     True or False?
+#1 !=0                  True
+#1 != 1                 False
+#0 != 1                 True
+#0 != 0                 False
+
+#==                     True or False?
+#1 == 0                 False
+#1 == 1                 True
+#0 == 1                 False
+#0 == 0                 True
