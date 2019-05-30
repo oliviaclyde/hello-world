cc15c12e1d2c4a491574a2de0b81a090cd36f43d
oliviaclyde <olivialclyde@gmail.com>
2019-05-30 06:25:49 -0600

    initial

---
+++ exercises/ex28.py
@@ -0,0 +1,32 @@
+#Boolean Practice           Answer
+#1. True and True           True
+#2. False and True          False
+#3. 1 == 1 and 2 == 1       False
+#4. "test" == "test"        True
+#5. 1 == 1 or 2 != 1        True
+#6. True and 1 == 1         True
+#7. False and 0 != 0        False
+#8. True or 1 == 1          True
+#9. "test" == "testing"     False
+#10. 1 != 0 and 2 == 1      False
+#11. "test" != "testing"    True
+#12. "test" == 1            False
+#13. not (True and False)   True
+#14. not (1 == 1 and 0 != 1) False
+#15. not (10 == 1 or 1000 = 1000)   False
+#16. not (1 != 10 or 3 == 4)        False
+#17. not ("testing" == "testing" and "Zed" == "Cool Guy")   True
+#18. 1 == 1 and (not ("testing" == 1 or 1 == 0))            True
+#19. "chunky" == "bacon" and (not (3 == 4 or 3 == 3))       False
+#20. 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))     False
+#variation
+# 3 != 4 and not ("testing" != "test" or "Python" == "Python")          False
+
+#Other equality operators (comparison operators) - used to compare two things to return True or False
+# !=    not equal
+# ==    equal to
+# >=    greater than or equal to
+# <=    less than or equal to
+# >     greater than
+# <     less than
+# 
