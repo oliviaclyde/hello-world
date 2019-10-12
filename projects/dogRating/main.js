d2ae19f9ce70ff6986887cfd63db4ab5e2e5f2cc
oliviaclyde <olivialclyde@gmail.com>
2019-10-11 20:29:52 -0600

    initial

---
+++ projects/dogRating/main.js
@@ -0,0 +1,47 @@
+// Javascript file for index.html
+// Using the dog API : https://dog.ceo/dog-api/documentation
+
+
+// List all breeds
+$(document).ready(function() {
+
+    $("button").click(function(){
+        $("p").hide();
+      });
+    });
+    </script>
+   
+    $('#Home').slideToggle(1000).slideToggle(1000);
+   
+    // var $dog = $('#dog');
+    // $.ajax({
+    //     type: 'GET',
+    //     url: 'https://dog.ceo/api/breeds/list/all',
+    //     success: function(dog) {
+        
+            error: function() {
+                alert('error loading list');
+            }
+// Post Request to my local drive - save dog breed + rating to Dashboard page
+    #('#selectDog').on('click', function() {
+            var selectDog = {
+                breed: $
+            }
+    //    Save the dog rating to local drive and needs to appear in Dashboard with rating
+        $.ajax({
+            type: 'POST',
+            // url: my local drive?
+            data: dog;
+            success: 
+        })
+    }
+
+
+    //         }
+    //     }
+    // })
+});
+
+
+// Random Image
+
