555dbd124f5d35c92df0baaf867bc17d0058d6a0
oliviaclyde <olivialclyde@gmail.com>
2019-10-14 20:42:35 -0600

    initial

---
+++ practice-python/Divisors.py
@@ -0,0 +1,21 @@
+# Create a program that asks the user for a number and then prints out a list of all the divisors of that number. 
+# (If you don’t know what a divisor is, it is a number that divides evenly into another number. 
+# For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+
+
+num = int(input("Please enter a number: "))
+
+newList = []
+
+i = 1
+
+while i < num:
+    # if i == 0:
+    #     pass
+    if num % i == 0:
+        newList.append(i)
+    else:
+        pass
+    i+=1
+
+print(newList)
