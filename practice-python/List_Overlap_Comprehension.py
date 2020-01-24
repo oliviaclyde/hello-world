635feb3755e9849db2b5b6d762d5da59e9517f72
oliviaclyde <olivialclyde@gmail.com>
2020-01-24 13:49:37 -0700

    initial

---
+++ practice-python/List_Overlap_Comprehension.py
@@ -0,0 +1,35 @@
+# Take two lists and write a program that returns a list that contains only the elements that are common between the lists (without duplicates).
+
+import random
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+
+def inList(): 
+    new_list = [item for item in a if item in b]
+
+
+    print(new_list)
+
+
+inList()
+
+# Now make the list generate random numbers
+
+print("I'm going to generate a list of two random numbers. Then I will tell you what numbers are common between them.")
+
+def randSample():
+    a = random.sample(range(100), 13)
+    b = random.sample(range(100), 7)
+
+    common_numbers = [item for item in a if item in b]
+
+    print(a)
+    print(b)
+    print("These are the common numbers between the two lists: ") 
+    print(common_numbers)
+
+
+randSample()
