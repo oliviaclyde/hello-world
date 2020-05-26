2b50465b8b783149febf8dd16ce627921b9fef41
oliviaclyde <olivialclyde@gmail.com>
2020-05-26 11:03:19 -0600

    initial

---
+++ practice-python/Dict_to_List.py
@@ -0,0 +1,16 @@
+# Practice converting a dictionary to a list
+
+friends = {"John": 34, "James": 54, "Allison": 45, "Tara": 23}
+
+print(friends)
+
+print(list(friends))
+
+
+print(friends.values())
+
+newList = list(friends.values())
+print(newList)
+
+print(friends.keys())
+print(friends.items())
