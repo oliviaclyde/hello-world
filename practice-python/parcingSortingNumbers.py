a0e8cfd836d4a4ab13da1e6a7897c3a80c75484e
oliviaclyde <olivialclyde@gmail.com>
2020-09-09 11:30:16 -0600

    using maps and enumerate

--- practice-python/parcingSortingNumbers.py
+++ practice-python/parcingSortingNumbers.py
@@ -5,13 +5,17 @@
 
 # Write a test to see if it will pass
 
+# versions = ["1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0"]
+
+versions = ["1.0.12", "1.0.2"]
+
 def parseString(l):
-    return ["1.0", "1.0.2", "1.0.12", "1.1.2", "1.3.3"]
+    parsed = [item.split(".") for item in l]
+    print(parsed)
+    listOfNums = [map(int, item) for item in parsed]
+
+    for index, value in enumerate(listOfNums):
+        print(list(value))
+
     
-print(parseString(["1"]))
-
-
-
-
-
-    
+parseString(versions)
